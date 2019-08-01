0aa2cbf726b9ec58f97adedf4e0a56c4eb9030ab
Adri1G <gauche.adrien@gmail.com>
2019-08-01 13:27:22 +0200

    Ajout code marquee

--- Arduino-Coin-Acceptor/Arduino-Coin-Acceptor.ino
+++ Arduino-Coin-Acceptor/Arduino-Coin-Acceptor.ino
@@ -1,26 +1,24 @@
 //https://bigdanzblog.wordpress.com/2015/01/12/interfacing-dg600f-coin-acceptor-to-arduino/
 /**@author Adri1G
- * 
- * This program read with RS232 the incoming value from the coin acceptor and send 
- * Pin map:
- * 2 (D2) Arduino on coin acceptor Signal
- * 
- * Switch configuration coin acceptor :
- * OFF: SW1, SW2, SW4
- * ON: SW3
- */
+   Pin map:
+   2 (D2) Arduino on coin acceptor Signal
+
+   Switch configuration coin acceptor :
+   OFF: SW1, SW2, SW4
+   ON: SW3
+*/
 
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
 #define RATIO_CURRENCY_COIN_ACCEPTOR_SIGNAL   5
 #define SENDING_INTERVAL                      250
-#define PART_PRICE                            10    // € value 100
+#define PART_PRICE                            10    // ratio € x 100
 #define LED_PIN                               13
 
 SoftwareSerial mySerial(2, 3);  // RX, TX
 
-int coin_value = 0;             //1€ = 100 int, to avoid double imprecision by design https://stackoverflow.com/questions/23137719/incorrect-rounding-of-currency-double-values
+volatile int  coin_value = 0;             //1€ = 100 int, to avoid double imprecision by design https://stackoverflow.com/questions/23137719/incorrect-rounding-of-currency-double-values
 unsigned long previous_Millis = 0;
 unsigned long current_Millis;
 bool          high_state_sending = false;
@@ -39,38 +37,37 @@ void setup() {
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);
 
-
 }
 
 void loop() {
-  //TODO pass receved value coin to interrup
+  //TODO past receved value coin to interrup
 
   // any input coming from coin acceptor?
   if (mySerial.available()) {
     // read input, which is a 1 byte integer
-    coin_value += mySerial.read()*RATIO_CURRENCY_COIN_ACCEPTOR_SIGNAL; //ratio to adapt the value send by the coin acceptor
-    Serial.print("test");
-    Serial.print("Monnaie reçue ");// FIXME past two time by this function
+    coin_value = mySerial.read() * RATIO_CURRENCY_COIN_ACCEPTOR_SIGNAL; //ratio to adapt the value send by the coin acceptor
+    Serial.print("Monnaie reçue ");
     Serial.println(coin_value);
   }
-
-    if (coin_value > 0)
-    {
-      current_Millis = millis();
-      //if there is some money  and state is low, send a rising edge
-      if((current_Millis - previous_Millis >= SENDING_INTERVAL ) && !high_state_sending){
-        coin_value -= PART_PRICE;
-        digitalWrite(4,HIGH);
-        digitalWrite(LED_PIN, HIGH);
-        high_state_sending = true;
-        previous_Millis = current_Millis;
-      }
+  /*
+  if (coin_value > 0)
+  {
+    current_Millis = millis();
+    //if there is some money  and state is low, send a rising edge
+    if ((current_Millis - previous_Millis >= SENDING_INTERVAL ) && !high_state_sending) {
+      coin_value -= PART_PRICE;
+      digitalWrite(4, HIGH);
+      digitalWrite(LED_PIN, HIGH);
+      high_state_sending = true;
+      previous_Millis = current_Millis;
+    }
     //else send a falling edge
-    if((current_Millis - previous_Millis >= SENDING_INTERVAL ) && high_state_sending){
-      digitalWrite(4,LOW);
-      digitalWrite(LED_PIN,LOW);
+    if ((current_Millis - previous_Millis >= SENDING_INTERVAL ) && high_state_sending) {
+      digitalWrite(4, LOW);
+      digitalWrite(LED_PIN, LOW);
       high_state_sending = false;
       previous_Millis = current_Millis;
     }
   }
+  */
 }
